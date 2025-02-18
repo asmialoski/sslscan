bcfa60d91888d786bdee5ee5abb0a351e2db7249
Alexandre Smialoski <alexandre.smialoski@outlook.com>
2025-02-18 18:55:15 +0000

    Update sslscan.sh - tls1_2 and tls1_1

--- sslscan.sh
+++ sslscan.sh
@@ -6,7 +6,7 @@ cat /dev/null > urls_open_ports
 #Verifica as portas abertas nas URLs passadas por parâmetro
 while IFS= read -r line; do
     #echo "Text read from file: $line"
-    LIST=$(nmap --script ssl-cert -p443,2443,3443,4443,5443,6443,7443,8443,9443,8080,8081,8082,2000-2010,3000-3010,4000-4010,5000-5010,6000-6010,7000-7010,8000-8010,9000-9010,14001,30000,50000-50100 $line | grep ' open ')
+    LIST=$(nmap --script ssl-cert -p443 $line | grep ' open ')
     for s in $LIST; do
         PORT=$(echo $s | cut -d/ -f1)
         echo "$line;$PORT"
@@ -23,6 +23,24 @@ while IFS= read -r line; do
     test=$(echo "x" | timeout 10 openssl s_client -servername $url -connect $url:$port | grep 'Secure Renegotiation')
     echo "$url;$port;$test"
 done < urls_open_ports >> urls_ssl
+
+
+while IFS= read -r line; do
+    #echo "Text read from file: $line"
+    url=$(echo $line | cut -d";" -f1)
+    port=$(echo $line | cut -d";" -f2)
+    test=$(echo "x" | timeout 10 openssl s_client -servername $url -connect $url:$port -tls1_2 | grep 'Secure Renegotiation')
+    echo "$url;$port;$test"
+done < urls_open_ports >> urls_ssl
+
+while IFS= read -r line; do
+    #echo "Text read from file: $line"
+    url=$(echo $line | cut -d";" -f1)
+    port=$(echo $line | cut -d";" -f2)
+    test=$(echo "x" | timeout 10 openssl s_client -servername $url -connect $url:$port -tls1_1 | grep 'Secure Renegotiation')
+    echo "$url;$port;$test"
+done < urls_open_ports >> urls_ssl
+
 
 cat /dev/null > urls_ssl_supported
 cat /dev/null > urls_certinfo
